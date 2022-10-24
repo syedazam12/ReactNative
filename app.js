47239565b9a01eb711d44265d54ea252176992ba
Syed Azam <azammasood2003@gmail.com>
2022-10-25 00:41:00 +0500

    created app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {Button, View, Text} from 'react-native'
+
+export default class App extends Component
+{
+    constructor(props)
+    {
+        super(props)
+    }
+
+    render()
+    {
+        return
+        (
+            <View>
+                <Text>Hello React Native</Text>
+            </View>
+        )
+    }
+}
